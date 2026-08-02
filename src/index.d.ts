2391c8643b108b078306eb71fbe1e7c0bf7123ff
Nicolas Morel <nicolas@morel.io>
2026-08-02 13:04:10 +0200

    feat: convert to ESM (#16)

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Generate a random file name within a given path and optional extension.
+ *
+ * @param path - The file path within to generate a temporary file.
+ * @param extension - File extension.
+ * @returns The generarted filename.
+ */
+export function uniqueFilename(path: string, extension?: string): string;
